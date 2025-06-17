d088ba4bcc4748ea60606bb93a6155ab1516fee8
deeeppraj <work.deepraj@gmail.com>
2025-06-17 13:06:08 +0530

    added logger.py

---
+++ netwoksecurity/logging/logger.py
@@ -0,0 +1,17 @@
+import os
+import sys
+import logging
+from datetime import datetime
+
+LOG_FILE = datetime.now().strftime("%Y:%M:%D:%H:%M:%S") + ".log"
+log = os.path.join(os.getcwd(),'Logs') 
+folder = os.makedirs(log, exist_ok= True)
+LOG_FILE_PATH = os.path.join(log, LOG_FILE)
+
+
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+
+)
