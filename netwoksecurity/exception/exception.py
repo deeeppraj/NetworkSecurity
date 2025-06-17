590419179d7df7237fdf29e97e626403d3849653
deeeppraj <work.deepraj@gmail.com>
2025-06-18 00:38:32 +0530

    aa

--- netwoksecurity/exception/exception.py
+++ netwoksecurity/exception/exception.py
@@ -17,14 +17,3 @@ class CustomException(Exception):
             error_message = f"Error occured in {self.file_name} ,  line number {self.line_no} , {str(self.error)}"
             return error_message
     
-
-if __name__ == "__main__":
-    try:
-        logging.info("Writting my try block")
-        a = 1/0
-        print("The divison will be throwing an exception")
-    
-    except Exception as e:
-         logging.info(f"Raising My CUstom Exception, error generated {str(e)}")
-         raise CustomException(e,sys)
-    
